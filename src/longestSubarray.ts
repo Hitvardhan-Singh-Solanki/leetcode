59c82d20e2ba15787611122bac1fa84c68fc7d41
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2025-07-30 02:59:33 +0200

    potd

--- src/longestSubarray.ts
+++ src/longestSubarray.ts
@@ -1,15 +1,11 @@
 export function longestSubarray(nums: number[]): number {
-  let maxNumber = nums.reduce((acc, val) => {
-    return Math.max(acc, val);
-  }, -Infinity);
-
+  let maxNumber = Math.max(...nums);
   let count = 0;
   let maxCount = 0;
   for (let i = 0; i < nums.length; i++) {
-    if (nums[i] === maxNumber) {
-      count++;
-      maxCount = Math.max(maxCount, count);
-    } else count = 0;
+    if (nums[i] === maxNumber) count++;
+    else count = 0;
+    maxCount = Math.max(maxCount, count);
   }
 
   return maxCount;
