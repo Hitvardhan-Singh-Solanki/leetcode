2341e62abde8879cb0c75a9f9352ce31457bed99
hitvardhan-singh-solanki <15267798+Hitvardhan-Singh-Solanki@users.noreply.github.com>
2025-08-14 03:42:02 +0200

    refactor: initialize numSet with predefined values for clarity and performance

--- src/largestGoodInteger.ts
+++ src/largestGoodInteger.ts
@@ -1,7 +1,18 @@
 export function largestGoodInteger(num: string): string {
-  const numSet = new Set();
+  const numSet = new Set([
+    '999',
+    '888',
+    '777',
+    '666',
+    '555',
+    '444',
+    '333',
+    '222',
+    '111',
+    '000',
+  ]);
   let res = '';
-  for (let i = 9; i >= 0; i--) numSet.add(String(i).repeat(3));
+
   for (let i = 0; i < num.length - 2; i++) {
     let check = num[i];
     for (let j = i + 1; j < i + 3; j++) check += num[j];
